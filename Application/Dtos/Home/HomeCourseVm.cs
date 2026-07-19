9be8915de827ae71024abb45f58810458dd9fd92
anhtuan <trinhanhtuan.contact@gmail.com>
2026-07-19 20:48:01 +0700

    feat(home): implement home view

---
+++ Application/Dtos/Home/HomeCourseVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.Home
+{
+    public class HomeCourseVm
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Thumbnail { get; set; }
+        public DateTime? PublishAt { get; set; }
+    }
+    public class HomeViewModel
+    {
+        public List<HomeCourseVm> PopularCourses { get; set; } = new List<HomeCourseVm>();
+        public List<HomeCourseVm> NewestCourses { get; set; } = new List<HomeCourseVm>();
+    }
+}
---
+++ Application/UseCases/HomeService.cs
@@ -0,0 +1,55 @@
+﻿using Application.Common.Interfaces;
+using Application.Dtos.Home;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases
+{
+    public interface IHomeService
+    {
+        Task<HomeViewModel> GetHomeDataAsync();
+    }
+
+    public class HomeService : IHomeService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly ICourseRepository _course;
+
+        public HomeService(IUnitOfWork uow, ICourseRepository course)
+        {
+            _uow = uow;
+            _course = course;
+        }
+
+        public async Task<HomeViewModel> GetHomeDataAsync()
+        {
+            var popularCourses = await _course.GetPopularCoursesAsync(4);
+            var newestCourses = await _course.GetNewestCoursesAsync(4);
+
+            return new HomeViewModel
+            {
+                PopularCourses = popularCourses.Select(c => new HomeCourseVm
+                {
+                    Id = c.Id,
+                    Title = c.Title,
+                    Description = c.Description,
+                    Thumbnail = c.Thumbnail,
+                    PublishAt = c.PublishAt ?? c.CreatedAt
+                }).ToList(),
+
+                NewestCourses = newestCourses.Select(c => new HomeCourseVm
+                {
+                    Id = c.Id,
+                    Title = c.Title,
+                    Description = c.Description,
+                    Thumbnail = c.Thumbnail,
+                    PublishAt = c.PublishAt ?? c.CreatedAt
+                }).ToList()
+            };
+        }
+    }
+}
